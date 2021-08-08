f0c9dea7f9da6b56e46d5302273bf94ab608ceeb
Carolina Reyes <carolina.reyes@sailpoint.com>
2021-08-08 12:04:26 -0500

    adding updated schema file

--- schema.sql
+++ schema.sql
@@ -32,12 +32,18 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 CREATE TABLE titles (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+	emp_no INT NOT NULL,
+	title VARCHAR (32) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-SELECT * FROM departments;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
